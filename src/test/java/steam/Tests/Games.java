b697bed99d1b225e6e598771dd64ff31a945a5a1
Даша <Teddi21girl@gmail.com>
2023-01-27 19:18:09 +0300

    still bad but working

--- src/test/java/steam/Tests/Games.java
+++ src/test/java/steam/Tests/Games.java
@@ -1,48 +1,59 @@
 package steam.Tests;
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import java.util.ArrayList;
 import java.util.List;
-
 import static com.codeborne.selenide.Selenide.*;
 
 public class Games extends BaseTest {
 
     @Test
     public void gameSearchSims() {
- //       Configuration.timeout=80000;
-        Configuration.pageLoadTimeout=3000;
+        // Configuration.pageLoadTimeout=3000;
         open("https://store.steampowered.com/");
         $("#store_nav_search_term").setValue("The sims").pressEnter();
         $(By.id("sort_by_trigger")).click();
         $(By.id("Price_ASC")).click();  // по возрастанию
-        // надо сделать проверку сортировки
 
-        List<String> links = new ArrayList<>();
-        ElementsCollection href = $$x("//div[@id='search_resultsRows']//a[@href]");
-        for (SelenideElement element : href) {
-            links.add(element.getAttribute("href"));
-
+        // обозначаем коллекцию элементов, собираем все ссылки на игры из поля и достаем из них атрибут с ценой
+        ElementsCollection href = $$x("//div[@id='search_resultsRows']//a[@href]//div[@data-price-final]");
+        List<Integer> price = new ArrayList<>();
+        for (int i = 0; i < href.size(); i++) {
+            String data = href.get(i).getAttribute("data-price-final");
+            // переводим список из стринг в инт
+            price.add(Integer.parseInt(data));
+        }
+        // наша проверка
+        for (int i = 4; i < price.size()-1; i++) {
+            if (price.get(i) > price.get(i + 1)) {
+                System.out.println("Фильтрация неверная");
+                break;
+            }
         }
     }
+
     @Test
     public void gameSearchGTA() {
         open("https://store.steampowered.com/");
         $("#store_nav_search_term").setValue("GTA").pressEnter();
         $("#sort_by_trigger").click();
-        $(By.id("Price_DESC")).click();  // по убыванию
+        $(By.id("Price_DESC")).click();// по убыванию
 
-        // надо сделать проверку сортировки
-
-        // обозначаем коллекцию элементов
+        // обозначаем коллекцию элементов, собираем все ссылки на игры из поля и достаем из них атрибут с ценой
         ElementsCollection href = $$x("//div[@id='search_resultsRows']//a[@href]//div[@data-price-final]");
-        List<String> price = new ArrayList<>();
-        for (int i = 0; i < href.size(); i++){
+        List<Integer> price = new ArrayList<>();
+        for (int i = 0; i < href.size(); i++) {
             String data = href.get(i).getAttribute("data-price-final");
-            price.add(data);
+            // переводим список из стринг в инт
+            price.add(Integer.parseInt(data));
+        }
+        // наша проверка
+        for (int i = 4; i < price.size()-1; i++) {
+            if (price.get(i) < price.get(i + 1)) {
+                System.out.println("Фильтрация неверная");
+                break;
+            }
         }
     }
 }
