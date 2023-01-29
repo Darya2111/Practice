c40e6fa01d0c458072a2399a10c46c2973848d52
Admin <dmitrychichlo@gmail.com>
2023-01-29 15:10:11 +0400

    comments

--- src/test/java/demo/qa/Pages/CheckoutPage.java
+++ src/test/java/demo/qa/Pages/CheckoutPage.java
@@ -5,19 +5,25 @@ import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CheckoutPage {
 
     // по идее с помощью этого я могла бы обращаться к локатору, но что-то не получается (
     private By element = By.className("table-responsive");
 
+    //TODO Изучи этот локатор , если надо почитай про форматирование строк в java
+    private String collectionOfElements = "//tbody/tr/td[contains(text(),'%s')]";
 
     public boolean IsTitleContains (String value){
         return $("#example-modal-sizes-title-lg").has(Condition.text(value));
     }
     public boolean IsStudentName (String value){
-        return $("Student Name").parent().has(Condition.text(value));
+        //TODO Создай метод, который будет делать аналогично
+        String xpath = String.format(collectionOfElements, value);
+        System.out.println(value);
+        System.out.println($x(xpath).getText());
+        return $x(xpath).getText().equals(value);
     }
     public boolean IsStudentEmail (String value){
         return $(byText("Student Email")).parent().has(text(value));
--- src/test/java/demo/qa/Tests/RegistrationForm.java
+++ src/test/java/demo/qa/Tests/RegistrationForm.java
@@ -13,11 +13,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class RegistrationForm extends BaseTest{
 
     @Test
-    void fillFormTest() {
+    void fillFormTest(){
         registrationFormPage.deleteBanAndFooter()
                             .setFirstName(firstName)
                             .setLastName(lastName)
                             .setEmail(email)
+                //TODO Погугли randomazer в java
                 // я хз как эти штуки сделать рандомными (((
                             .setGender("Female")
                             .setMobile("1234567890")
@@ -35,15 +36,15 @@ public class RegistrationForm extends BaseTest{
         // при проверке ошибка, данные не совпадают, возможно из-за faker(
 
         assertThat("Title не совпадает",checkoutPage.IsTitleContains("Thanks for submitting the form"));
-    //    assertThat("Name не совпадает",checkoutPage.IsStudentName(firstName));
-    //    assertThat("Email не совпадает",checkoutPage.IsStudentEmail(email));
-        assertThat("Gender не совпадает",checkoutPage.IsGender("Female"));
-        assertThat("Mobile не совпадает",checkoutPage.IsMobile("1234567890"));
-        assertThat("Date Of Birth не совпадает",checkoutPage.IsDateOfBirth("Date of Birth 21 November,1998"));
-        assertThat("Subject не совпадает",checkoutPage.IsSubject("Math"));
-        assertThat("Hobbie не совпадает",checkoutPage.IsHobbie("Reading"));
-    //    assertThat("Address не совпадает",checkoutPage.IsAddress(currentAddress));
-        assertThat("State and City не совпадает",checkoutPage.IsStateAndCity("NCR Delhi"));
+        assertThat("Name не совпадает",checkoutPage.IsStudentName(firstName + " " + lastName));
+//        assertThat("Email не совпадает",checkoutPage.IsStudentEmail(email));
+//        assertThat("Gender не совпадает",checkoutPage.IsGender("Female"));
+//        assertThat("Mobile не совпадает",checkoutPage.IsMobile("1234567890"));
+//        assertThat("Date Of Birth не совпадает",checkoutPage.IsDateOfBirth("Date of Birth 21 November,1998"));
+//        assertThat("Subject не совпадает",checkoutPage.IsSubject("Math"));
+//        assertThat("Hobbie не совпадает",checkoutPage.IsHobbie("Reading"));
+//    //    assertThat("Address не совпадает",checkoutPage.IsAddress(currentAddress));
+//        assertThat("State and City не совпадает",checkoutPage.IsStateAndCity("NCR Delhi"));
 
     }
 }
